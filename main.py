48424e8cba1a4dcb33f86a25f9c7156a6805ee0c
lihuangzheng <jerryzheng0131@gmail.com>
2022-12-16 22:25:35 -0500

    add hello

--- main.py
+++ main.py
@@ -4,18 +4,31 @@ import os
 import psutil as psutil
 import requests as requests
 
-# fetch api response and it's data then print it
+# print out current user's name
+print("Hello, " + os.getlogin() + "!")
+
+
+# print out current day with time
+print("Today is: " + os.popen('date').read())
+
+
+# create line divider def
+def line_divider():
+    print("--------------------------------------------------")
+
+
+# fetch api response
 response = requests.get("https://zenquotes.io/api/quotes/")
+
+line_divider()
 # print json data of 'q' key
 print("Quote: " + response.json()[0]['q'])
 # print json data of author key with by at first then author name
 print("by " + response.json()[0]['a'])
+line_divider()
 
 # force garbage collection for python
 gc.collect()
-
-# print out current day with time
-print("Today is: " + os.popen('date').read())
 
 # print out the memory usage of the current process in MB with MB at the end
 print("Memory usage: ", str(psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024) + " MB")
